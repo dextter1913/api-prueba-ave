47cf5d66382178281f2d7fed7fbae2d95333424e
dextter1913 <dextter1913@gmail.com>
2023-08-03 13:47:50 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://api-prueba-ave-production.up.railway.app/',
     plugins: [
         laravel({
             input: [
