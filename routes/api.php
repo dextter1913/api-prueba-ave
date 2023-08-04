d1990b95c6ab99498f3c1e86f3dc218f214d2ec0
dextter1913 <dextter1913@gmail.com>
2023-08-04 06:25:19 -0500

    fix: solucionando mismo inconveniente con api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\CreateUserController;
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\CreateUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
