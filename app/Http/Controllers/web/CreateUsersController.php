b2e8ff126f85ea0dfb37838487b232505ee58614
dextter1913 <dextter1913@gmail.com>
2023-08-03 21:01:47 -0500

    feat: agregando creacion de usuario desde el front

--- app/Http/Controllers/web/CreateUsersController.php
+++ app/Http/Controllers/web/CreateUsersController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class CreateUsersController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('modulos.form-client');
+    }
+
+    public function create(Request $request)
+    {
+    }
 }
--- database/migrations/2023_08_03_154931_create_usuarios_table.php
+++ database/migrations/2023_08_03_154931_create_usuarios_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('documento')->comment('Documento de el cliente');
+            $table->string('nombre')->comment('Nombre del cliente');
+            $table->string('apellido')->comment('Apellido del cliente');
+            $table->string('ciudad')->comment('Ciudad de residencia del cliente');
+            $table->string('direccion')->comment('Direccion de vivienda del cliente');
+            $table->string('celular')->comment('Celular del cliente');
+            $table->text('documento_base64')->comment('base 64 de el documento de identidad del cliente');
+            $table->string('correo')->comment('Correo electronico del cliente');
+            $table->string('cliente_creado_por')->comment('Usuario de la persona que inserto el cliente');
+            $table->string('cliente_eliminado_por')->comment('Usuario de la persona que elimino el cliente');
             $table->timestamps();
         });
     }
@@ -25,3 +35,15 @@ return new class extends Migration
         Schema::dropIfExists('usuarios');
     }
 };
+// CREATE TABLE IF NOT EXISTS `usuarios` (
+//     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+//     `documento` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `apellido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `ciudad` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `direccion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `celular` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `cedula_base64` text COLLATE utf8mb4_unicode_ci NOT NULL,
+//     `correo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+//     PRIMARY KEY (`id`),
+//     UNIQUE KEY `usuarios_correo_unique` (`correo`)
